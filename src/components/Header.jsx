ff8bdd73fad48c9b89f41626f6a48286c658b748
Kelly Ren <renkelei.kelly@gmail.com>
2020-11-27 15:57:58 +0200

    adjusted header content

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,28 @@
-import { Link } from "gatsby"
+import { Link } from "react-scroll"
 import PropTypes from "prop-types"
 import React from "react"
 
-
 const ListLink = props => (
   <li style={{ display: `inline-block`, marginRight: `1rem` }}>
-    <Link to={props.to}>{props.children}</Link>
+    <Link to={props.to} smooth duration={1000}>
+      {props.children}
+    </Link>
   </li>
 )
 
-const Header = ({ siteTitle }) => (
-  <header style={{ marginBottom: `1.5rem` }}>
-          <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
-            <h3 style={{ display: `inline` }}>
-              {siteTitle}
-            </h3>
-          </Link>
-          <ul style={{ listStyle: `none`, float: `right` }}>
-            <ListLink to="/">Home</ListLink>
-            <ListLink to="/about/">About</ListLink>
-            <ListLink to="/contact/">Contact</ListLink>
-          </ul>
-        </header>
-)
+const Header = ({ siteTitle }) => {
+  return (
+    <header style={{ marginBottom: `1.5rem` }}>
+      <Link to="/" style={{ textShadow: `none`, backgroundImage: `none` }}>
+        <h3 style={{ display: `inline` }}>{siteTitle}</h3>
+      </Link>
+      <ul style={{ listStyle: `none`, float: `right` }}>
+        <ListLink to="about">About</ListLink>
+        <ListLink to="contact">Contact</ListLink>
+      </ul>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
