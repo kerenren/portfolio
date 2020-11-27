acd777e1ce207dac00f542d14586825dff019e45
Kelly Ren <renkelei.kelly@gmail.com>
2020-11-27 21:27:19 +0200

    updated about profile position and style

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,32 +1,31 @@
-import React from "react";
-import { useStaticQuery, graphql } from "gatsby";
-import Img from "gatsby-image";
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
       file(relativePath: { eq: "kelly-profile.png" }) {
         childImageSharp {
-          fluid(maxWidth: 300, quality: 100) {
-            ...GatsbyImageSharpFluid_noBase64
+          fixed(width: 260, quality: 100) {
+            ...GatsbyImageSharpFixed_noBase64
           }
         }
       }
     }
   `)
 
-  if (!data?.file?.childImageSharp?.fluid) {
+  if (!data?.file?.childImageSharp?.fixed) {
     return <div>Picture not found</div>
   }
 
   return (
-    <>
-      <Img
-        fluid={data.file.childImageSharp.fluid}
-        placeholderStyle={{ visibility: "hidden" }}
-        alt="Profile picture"
-      />
-    </>
+    <Img
+      fixed={data.file.childImageSharp.fixed}
+      placeholderStyle={{ visibility: "hidden" }}
+      alt="Profile picture"
+      style={{borderRadius:"50%"}}
+    />
   )
 }
 
