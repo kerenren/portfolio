f8d246b3b3bfa202ed69b38dd49db8eec3c73c2a
Kelly Ren <renkelei.kelly@gmail.com>
2020-11-27 20:28:11 +0200

    removed layout wrapper margin and padding for main content

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql, Link } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Header from "./Header.jsx"
 import Footer from "./Footer.jsx"
@@ -22,13 +22,7 @@ const Layout = ({ children }) => {
       <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
         <Header siteTitle={data.site.siteMetadata.title} />
       </div>
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <div>
         <main>{children}</main>
         <Footer />
       </div>
