d909db0d6ab4a493bdf43cae3b6891cf4fdeafae
Kelly Ren <renkelei.kelly@gmail.com>
2020-11-27 20:26:29 +0200

    implemented custom style to app

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,19 +2,21 @@ import React from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
 
 import Layout from "../components/Layout.jsx"
-import Image from "../components/Image.jsx"
 import SEO from "../components/seo"
 import Hero from "../components/Hero.jsx"
 import About from "../components/About.jsx"
 import Contact from "../components/Contact.jsx"
+import "../styles/custom.css"
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
+  <>
     <Hero />
-    <About />
-    <Contact />
-  </Layout>
+    <Layout>
+      <SEO title="Home" />
+      <About />
+      <Contact />
+    </Layout>
+  </>
 )
 
 export default IndexPage
