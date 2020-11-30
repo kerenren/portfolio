e32ab1f1929c5e433f368bbf5135557bf8dee5d9
Kelly Ren <renkelei.kelly@gmail.com>
2020-11-30 18:37:12 +0200

    added projecs component

---
+++ src/components/Projects.jsx
@@ -0,0 +1,114 @@
+import React, { useContext, useState, useEffect } from "react"
+import Fade from "react-reveal/Fade"
+import { Container, Row, Col } from "react-bootstrap"
+import Tilt from "react-tilt"
+
+import Title from "./Title.jsx"
+import ProjectImg from "./ProjectImg.jsx"
+import PortfolioContext from "../library/context"
+import styles from "../styles/sections/projects.module.css"
+
+const Projects = () => {
+  const { projects } = useContext(PortfolioContext)
+
+  const [isDesktop, setIsDesktop] = useState(false)
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    if (window.innerWidth > 769) {
+      setIsDesktop(true)
+      setIsMobile(false)
+    } else {
+      setIsMobile(true)
+      setIsDesktop(false)
+    }
+  }, [])
+
+  return (
+    <section id="projects" className={styles.projects}>
+      <Container>
+        <Title title="Projects" />
+        {projects.map(project => {
+          const { title, info, info2, url, repo, img, id } = project
+
+          return (
+            <Row key={id}>
+              <Col lg={4} sm={12}>
+                <Fade
+                  left={isDesktop}
+                  bottom={isMobile}
+                  duration={1000}
+                  delay={500}
+                  distance="30px"
+                >
+                  <div className={styles.projectWrapperText}>
+                    <h3>{title}</h3>
+                    <p>{info}</p>
+                    <p>{info2}</p>
+                    <a
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="cta-btn cta-btn-hero"
+                      href={url || "#!"}
+                    >
+                      See Live
+                    </a>
+
+                    {repo && (
+                      <a
+                        target="_blank"
+                        rel="noopener noreferrer"
+                        className="cta-btn text-color-main"
+                        href={repo}
+                      >
+                        Source Code
+                      </a>
+                    )}
+                  </div>
+                </Fade>
+              </Col>
+              <Col lg={8} sm={12}>
+                <Fade
+                  right={isDesktop}
+                  bottom={isMobile}
+                  duration={1000}
+                  delay={1000}
+                  distance="30px"
+                >
+                  <div className={styles.projectWrapperImage}>
+                    <a
+                      href={url || "#!"}
+                      target="_blank"
+                      aria-label="Project Link"
+                      rel="noopener noreferrer"
+                    >
+                      <Tilt
+                        options={{
+                          reverse: false,
+                          max: 8,
+                          perspective: 1000,
+                          scale: 1,
+                          speed: 300,
+                          transition: true,
+                          axis: null,
+                          reset: true,
+                          easing: "cubic-bezier(.03,.98,.52,.99)",
+                        }}
+                      >
+                        <div data-tilt className="thumbnail rounded">
+                          <ProjectImg alt={title} filename={img} />
+                        </div>
+                      </Tilt>
+                    </a>
+                  </div>
+                </Fade>
+              </Col>
+            </Row>
+          )
+        })}
+      </Container>
+    </section>
+  )
+}
+
+export default Projects
