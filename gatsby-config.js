346118a027bd50a09f18e85a2e1ae80f6349e03f
Kelly Ren <renkelei.kelly@gmail.com>
2020-12-02 01:51:04 +0200

    rename the icon anme to lowercase

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/Kelly-icon.png`, 
+        icon: `src/images/kelly-icon.png`, 
       },
     },
     {
