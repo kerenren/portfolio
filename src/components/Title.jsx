10221020550c0e487700ced572e324cfdf47f330
Kelly Ren <renkelei.kelly@gmail.com>
2020-12-01 16:07:22 +0200

    updating classname

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 const Title = ({ title }) => (
   <Fade bottom duration={1000} delay={300} distance="0px">
-    <h2 className="about-title">{title}</h2>
+    <h2 className="section-title">{title}</h2>
   </Fade>
 );
 
