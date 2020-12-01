189073d237b316ffc96480619dbf954772cdd036
Kelly Ren <renkelei.kelly@gmail.com>
2020-12-01 19:03:30 +0200

    implemented responsive design to contanct section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,24 +1,25 @@
-import React from "react"
-import Fade from "react-reveal/Fade"
 import { Container } from "react-bootstrap"
+import Fade from "react-reveal/Fade"
+import React from "react"
 
+import styles from "../styles/sections/contact.module.css"
 import Title from "./Title.jsx"
 
 
 const Contact = () => {
   return (
-    <section id="contact">
+    <section id="contact" className={styles.contact}>
       <Container>
         <Title title="Contact" />
         <Fade bottom duration={1000} delay={800} distance="30px">
-          <div className="contact-wrapper">
-            <p className="contact-wrapper__text">
+          <div className={styles.contactWrapper}>
+            <p className={styles.contactWrapperText}>
               'Would you like to work with me? Awesome!'
             </p>
             <a
               target="_blank"
               rel="noopener noreferrer"
-              className="cta-btn cta-btn--resume"
+              className="cta-btn hvr-shutter-out-horizontal cta-btn-resume"
               href="mailto:"
             >
               Let's talk
