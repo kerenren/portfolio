ad2924bd02af26f47b42ad86ba90b56a760550c4
Kelly Ren <renkelei.kelly@gmail.com>
2020-12-02 11:41:33 +0200

    enabled fluid and fixed nav menue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
+import React, { useState, useEffect } from "react"
 import { Link } from "react-scroll"
 import PropTypes from "prop-types"
-import React from "react"
 
 import styles from "../styles/sections/header.module.css"
 import HeaderImg from "./HeaderImg.jsx"
@@ -14,12 +14,43 @@ const ListLink = props => (
 )
 
 const Header = ({ siteTitle }) => {
+  const vh = Math.max(
+    document.documentElement.clientHeight || 0,
+    window.innerHeight || 0
+  )
+  const [scrollState, setScrollState] = useState("buttom")
+
+  const checkScrollTop = () => {
+    const scrolled = document.scrollingElement.scrollTop
+    if (scrolled >= vh) {
+      if (scrollState !== "top") {
+        setScrollState("top")
+      }
+    } else {
+      if (scrollState !== "buttom") {
+        setScrollState("buttom")
+      }
+    }
+  }
+
+  useEffect(() => {
+    document.addEventListener("scroll", checkScrollTop)
+    return () => {
+      document.removeEventListener("scroll", checkScrollTop)
+    }
+  })
+
   return (
-    <header className={styles.header}>
+    <header
+      className={`${styles.header} ${
+        scrollState !== "top" ? styles.buttomNav : styles.topNav
+      }`}
+    >
       <Link to="hero" className={styles.headerIcon} smooth duration={1000}>
-        <HeaderImg alt={siteTitle}/>
+        <HeaderImg alt={siteTitle} />
       </Link>
       <ul classname={styles.navItems}>
+        <ListLink to="hero">Home</ListLink>
         <ListLink to="about">About</ListLink>
         <ListLink to="projects">Projects</ListLink>
         <ListLink to="contact">Contact</ListLink>
