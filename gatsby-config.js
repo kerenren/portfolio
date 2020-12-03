ef734dff4261ed291529704e30b0066091adb9a3
Kelly Ren <renkelei.kelly@gmail.com>
2020-12-03 15:58:32 +0200

    removing icon from gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,29 +25,6 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/kelly-icon.png`,
-        icons: [
-          {
-            src: "favicons/icon-48x48.png",
-            sizes: "48x48",
-            type: "image/png",
-          },
-          {
-            src: "favicons/icon-72x72.png",
-            sizes: "72x72",
-            type: "image/png",
-          },
-          {
-            src: "favicons/icon-96x96.png",
-            sizes: "96x96",
-            type: "image/png",
-          },
-          {
-            src: "favicons/icon-144x144.png",
-            sizes: "144x144",
-            type: "image/png",
-          },
-        ],
       },
     },
     {
