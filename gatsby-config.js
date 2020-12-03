9fc4a1d5ca9dfa004a61cd36c5558259d8023fa6
Kelly Ren <renkelei.kelly@gmail.com>
2020-12-03 23:07:28 +0200

    updating favicon config

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,30 +24,8 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/kelly-icon.png`,
-        icons: [
-          {
-            src: "icons/icon-48x48.png",
-            sizes: "48x48",
-            type: "image/png",
-          },
-          {
-            src: "icons/icon-72x72.png",
-            sizes: "72x72",
-            type: "image/png",
-          },
-          {
-            src: "icons/icon-96x96.png",
-            sizes: "96x96",
-            type: "image/png",
-          },
-          {
-            src: "icons/icon-144x144.png",
-            sizes: "144x144",
-            type: "image/png",
-          },
-        ],
+        display: `standalone`,
+        icon: `src/images/favicon.png`,
       },
     },
     {
