1f8237c6dff733bfd1311f59a42da03a702f78f5
Kelly Ren <renkelei.kelly@gmail.com>
2020-12-03 15:46:46 +0200

    updating public folder

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,17 +25,37 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/kelly-icon.png`, 
+        icon: `src/kelly-icon.png`,
+        icons: [
+          {
+            src: "favicons/icon-48x48.png",
+            sizes: "48x48",
+            type: "image/png",
+          },
+          {
+            src: "favicons/icon-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "favicons/icon-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "favicons/icon-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+        ],
       },
     },
     {
       resolve: `gatsby-plugin-google-fonts`,
       options: {
-        fonts: [
-          `Montserrat pro\:400,500,600,700`,
-        ],
-        display: 'swap'
-      }
+        fonts: [`Montserrat pro\:400,500,600,700`],
+        display: "swap",
+      },
     },
   ],
 }
