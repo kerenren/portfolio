4cd97d3082db292e3cb4b6dd4f415f2f96bfc9bc
Kelly Ren <renkelei.kelly@gmail.com>
2020-12-03 15:19:10 +0200

    updating gatsby-config for favicon path

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/kelly-icon.png`, 
+        icon: `${__dirname}/src/images/kelly-icon.png`, 
       },
     },
     {
