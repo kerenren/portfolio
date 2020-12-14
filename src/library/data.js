10fed4597c28e855e8ca026af640439fba9cee08
Kelly Ren <renkelei.kelly@gmail.com>
2020-12-14 22:26:04 +0200

    updating project data

--- src/library/data.js
+++ src/library/data.js
@@ -7,7 +7,7 @@ export const projectsData = [
     img: "ProjectTurboKanban.jpg",
     title: "Turbo Kanban",
     info:
-      "A full stack web app using RESTful API to monitor real time cloud bots logs and presented in a bots dashboard, to retrive logs by bot id, to upload files and modules and to excute sql commands per types by simply selecting options from a user friendly form.",
+      "A full stack web app using RESTful API to monitor real time cloud bots logs and presented in a bots dashboard, to retrieve logs by bot id, to upload files and modules and to execute sql commands per types by simply selecting options from a user friendly form.",
     info2: "It's built with React.js, axios, python, flask, MySql database",
     url: "./",
     repo: "https://github.com/kerenren/Turbo-Kanban",
@@ -18,7 +18,7 @@ export const projectsData = [
     title: "Hogwarts CRM APP",
     info:
       "An Hogwarts CRM (customer relationship management) full stack app using React, Axios, Python and Flask.",
-    info2: "The CRM uses nivo chart for data visulizaion, and uses MongoDb to store and manipulate the data. ",
+    info2: "The CRM uses nivo chart for charts visualization, and uses MongoDb to store and manipulate the data. ",
     url: "./",
     repo: "https://github.com/kerenren/hogwarts_crm_react",
   },
@@ -27,9 +27,9 @@ export const projectsData = [
     img: "project-stock-exchange.jpg",
     title: "Vanilla JavaScript Stock Exchange Web App.",
     info:
-      "A multipage stock exchange data website. Used Vanila Javascript completly to build html per module.",
+      "A multipage stock exchange data website. Used Vanilla Javascript completely to build html per module.",
     info2:
-      "Built with Node.js, Express, RESTful API, chart.js, MongoDB. Hosted by Heroku. Users are able to search stock exchange companies by symbol, query is hilighted in the search result. Public companies details and stock quotes are able to be retrived. ",
+      "Built with Node.js, Express, RESTful API, chart.js, MongoDB. Hosted by Heroku. Users are able to search stock exchange companies by symbol, query is highlighted in the search result. Public companies details and stock quotes are able to be retrieved. ",
     url: "https://stock-exchange-js.herokuapp.com/index.html",
     repo: "https://github.com/kerenren/js-project-2-stock-exchange",
   },
@@ -38,7 +38,7 @@ export const projectsData = [
     img: "project-tree-whisper.jpg",
     title: "TheTreeWhisper",
     info:
-      "a 48 hours Hackathon project which uses React, Python, Flask and AI algorithm to detect if plants are healthy or not from the image recogonition.",
+      "a 48 hours Hackathon project which uses React, Python, Flask and AI algorithm to detect if plants are healthy or not from the image recognition.",
     info2: "Built with React, Axios, Flask, tensorflow.",
     url: "./",
     repo: "https://github.com/kerenren/TheTreeWhisperer",
@@ -49,12 +49,12 @@ export const projectsData = [
     title: "React Micro blogging",
     info: "A fully functional micro blogging web app (like twitter).",
     info2: "Built with React, and hosted by Google Firebase.",
-    url: "https://react-micro-blogging-kerenren.web.app/",
+    url: "https://react-micro-blogging-kerenren.web.app/home",
     repo: "https://github.com/kerenren/news_portal",
   },
   {
     id: nanoid(),
-    img: "ProjectTurboKanban.jpg",
+    img: "project-news-portal.jpg",
     title: "News Portal",
     info: "News portal which present news feeds from news API and Rss feeds.",
     info2: "Built with React, chakra UI.",
@@ -85,7 +85,7 @@ export const projectsData = [
     id: nanoid(),
     img: "project-portfolio-vanilla.jpg",
     title: "Vanilla JavaScript portfolio",
-    info: "A self learning project before starting full stack bootcamp.",
+    info: "A self learning project before starting my study in Isral Tech Challenge Full Stack Web Development bootcamp.",
     info2: "Built with HTML, CSS and vanilla Javascript.",
     url: "https://kerenren.github.io/israel_tech_challenge/Documents/GitHub/Ren_Kelei_ITCPrecourse/index.html",
     repo: "https://github.com/kerenren/israel_tech_challenge",
